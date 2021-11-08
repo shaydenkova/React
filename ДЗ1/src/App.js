7ea155df9602e7ebc8633140dbba9f59cff0131f
shaydenkova <82531349+shaydenkova@users.noreply.github.com>
2021-11-08 22:01:28 +0200

    Add files via upload

---
+++ ДЗ1/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return ( 
+    <div>
+      <img src="./pic.jpg"  width="189" height="255" alt="lorem"></img>
+      <p>Добрый вечер!</p>
+    </div>
+    );
+}
+
+export default App;
