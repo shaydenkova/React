e63a316e9d3a99f984a945d114f6849928f8713c
shaydenkova <82531349+shaydenkova@users.noreply.github.com>
2021-12-11 00:37:24 +0200

    Add files via upload

---
+++ Homework3/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import NewsPage from './NewsPage';
+// import NewsFilters from './NewsFilters';
+
+ReactDOM.render(
+    <NewsPage />,
+  document.getElementById('newsPage')
+);
+
+
